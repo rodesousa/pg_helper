8a539d4a76e3c56f089d664cb4aa225ac7376d0c
Roberto Barros De Sousa <roberto.barros-de-sousa@arkhn.com>
2022-01-17 16:11:31 +0100

    feat: add percent_of_times_index_used
    Signed-off-by: Roberto Barros De Sousa <roberto.barros-de-sousa@arkhn.com>

--- lib/commands.ex
+++ lib/commands.ex
@@ -95,4 +95,20 @@ defmodule PgHelper.Commands do
     """
     |> query
   end
+
+  def percent_of_times_index_used do
+    """
+    SELECT
+    relname,
+    100 * idx_scan / (seq_scan + idx_scan) percent_of_times_index_used,
+    n_live_tup rows_in_table
+    FROM
+    pg_stat_user_tables
+    WHERE
+    seq_scan + idx_scan > 0
+    ORDER BY
+    n_live_tup DESC;
+
+    """
+  end
 end
